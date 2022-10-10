adc9eaa5184d9078605e56da2641e4c238c77621
sehnryr <youn@melois.dev>
2022-10-10 22:31:10 +0200

    format index

--- index.php
+++ index.php
@@ -1,45 +1,45 @@
-<head>
+<!DOCTYPE html>
+<html lang="fr">
+
 <body>
-<navbar><img class="Sign" src="public_html/img/SignB.png"></navbar>
-<link href="public_html/css/style.css" rel="stylesheet">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<div class="container"></div>
+    <navbar><img class="Sign" src="public_html/img/SignB.png"></navbar>
+    <link href="public_html/css/style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <div class="container"></div>
     <img src="public_html/img/InterPromo.png">
-<input class="ButtonDayNight" type="image" src="public_html/img/DAYnNGH.png" onclick="myFunction()" /> 
-<script>
-    window.IsDark = 0;
-    function myFunction() {
-       document.body.classList.toggle("LightMode");
-       if (window.IsDark == 0){
-            document.querySelector('.vertical_menu').style.backgroundColor = 'rgb(211, 211, 211)';
-            var Choices = document.querySelectorAll('.vertical_menu a');
-            Choices.forEach(choice => {
-                choice.style.color = 'rgb(14, 14, 14)';
-            });
-            document.querySelector('.sign').setAttribute('src',"public_html/img/SignN.png")
-            window.IsDark = 1;
-            console.log("White")
-       }
-        else{
-            document.querySelector('.vertical_menu').style.backgroundColor = 'rgb(44, 43, 43)';
-            var Choices = document.querySelectorAll('.vertical_menu a');
-            Choices.forEach(choice => {
-                choice.style.color = 'rgb(193, 193, 193)';
-            });
-            document.querySelector('.sign').setAttribute('src',"public_html/img/SignB.png")
-            window.IsDark = 0;
-            console.log("Dark");
+    <input class="ButtonDayNight" type="image" src="public_html/img/DAYnNGH.png" onclick="myFunction()" />
+    <script>
+        window.IsDark = 0;
+
+        function myFunction() {
+            document.body.classList.toggle("LightMode");
+            if (window.IsDark == 0) {
+                document.querySelector('.vertical_menu').style.backgroundColor = 'rgb(211, 211, 211)';
+                var Choices = document.querySelectorAll('.vertical_menu a');
+                Choices.forEach(choice => {
+                    choice.style.color = 'rgb(14, 14, 14)';
+                });
+                document.querySelector('.sign').setAttribute('src', "public_html/img/SignN.png")
+                window.IsDark = 1;
+                console.log("White")
+            } else {
+                document.querySelector('.vertical_menu').style.backgroundColor = 'rgb(44, 43, 43)';
+                var Choices = document.querySelectorAll('.vertical_menu a');
+                Choices.forEach(choice => {
+                    choice.style.color = 'rgb(193, 193, 193)';
+                });
+                document.querySelector('.sign').setAttribute('src', "public_html/img/SignB.png")
+                window.IsDark = 0;
+                console.log("Dark");
+            }
         }
-    }
-</script>
-<div class="vertical_menu" name="MenuV">
-    <a href="#" class="active">Acceuil</a>
-    <a href="#">Basket</a>
-    <a href="#">Hand</a>
-    <a href="#">Bad</a>
-    <a href="#">Volley</a>
-    <a href="#">Futsal</a>
-</div>
-
+    </script>
+    <div class="vertical_menu" name="MenuV">
+        <a href="#" class="active">Acceuil</a>
+        <a href="#">Basket</a>
+        <a href="#">Hand</a>
+        <a href="#">Bad</a>
+        <a href="#">Volley</a>
+        <a href="#">Futsal</a>
+    </div>
 </body>
-
