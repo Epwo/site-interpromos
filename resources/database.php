2908ac2341d084c56275f216fc37d95830f2f679
sehnryr <youn@melois.dev>
2022-10-10 21:04:52 +0200

    add user creation method

--- resources/database.php
+++ resources/database.php
@@ -7,6 +7,7 @@
  */
 
 require_once 'config.php';
+require_once 'library/exceptions.php';
 
 /**
  * Collection of methods to communicate with the database.
@@ -28,5 +29,43 @@ class Database
             DB_USER,
             DB_PASSWORD
         );
+    }
+
+    /**
+     * Create an user in the database and return a bool to result.
+     * 
+     * @param string $name     Name of the user. 
+     * @param string $email    Email of the user.
+     * @param string $password Password of the user.
+     */
+    public function createUser(string $name, string $email, string $password): bool
+    {
+        // test if user already exists
+        $request = 'SELECT * FROM users
+                        WHERE email = :email';
+
+        $statement = $this->PDO->prepare($request);
+        $statement->bindParam(':email', $email);
+        $statement->execute();
+
+        $result = $statement->fetch(PDO::FETCH_OBJ);
+
+        if ($result) {
+            throw new DuplicateEmailException('Email already exists.');
+        }
+
+        // create password hash to store in database
+        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+        // create user
+        $request = 'INSERT INTO users 
+                        ("name", email, password_hash)
+                        VALUES (:name, :email, :password_hash)';
+
+        $statement = $this->PDO->prepare($request);
+        $statement->bindParam(':name', $name);
+        $statement->bindParam(':email', $email);
+        $statement->bindParam(':password_hash', $password_hash);
+        return $statement->execute();
     }
 }
