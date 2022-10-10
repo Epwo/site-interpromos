3724204509edd6d49f0014c1f3cb877b128ee8f9
sehnryr <youn@melois.dev>
2022-10-10 21:46:30 +0200

    add user deletion + credentials verification methods

--- resources/database.php
+++ resources/database.php
@@ -32,11 +32,57 @@ class Database
     }
 
     /**
+     * Gets the password hash of a user.
+     * 
+     * @param string $email
+     * 
+     * @return ?string The password hash if exists.
+     */
+    public function getUserPasswordHash(string $email): ?string
+    {
+        $email = strtolower($email);
+
+        $request = 'SELECT password_hash FROM users 
+                        WHERE email = :email';
+
+        $statement = $this->PDO->prepare($request);
+        $statement->bindParam(':email', $email);
+        $statement->execute();
+
+        $result = $statement->fetch(PDO::FETCH_OBJ);
+
+        if (!$result) {
+            return NULL;
+        }
+
+        return $result->password_hash;
+    }
+
+    /**
+     * Verifies the user credentials.
+     * 
+     * @param string $email
+     * @param string $password
+     * 
+     * @return bool
+     */
+    public function verifyUserCredentials(
+        string $email,
+        string $password
+    ): bool {
+        $password_hash = $this->getUserPasswordHash($email);
+        return !empty($password_hash) &&
+            password_verify($password, $password_hash);
+    }
+
+    /**
      * Create an user in the database and return a bool to result.
      * 
      * @param string $name     Name of the user. 
      * @param string $email    Email of the user.
      * @param string $password Password of the user.
+     * 
+     * @throws DuplicateEmailException Error thrown if the email is already used
      */
     public function createUser(string $name, string $email, string $password): bool
     {
@@ -66,6 +112,29 @@ class Database
         $statement->bindParam(':name', $name);
         $statement->bindParam(':email', $email);
         $statement->bindParam(':password_hash', $password_hash);
+        return $statement->execute();
+    }
+
+    /**
+     * Deletes a user.
+     * 
+     * @param string $email
+     * @param string $password
+     * 
+     * @throws AuthenticationException
+     */
+    public function deleteUser(string $email, string $password): bool
+    {
+        // test if the credentials are correct
+        if (!$this->verifyUserCredentials($email, $password)) {
+            throw new AuthenticationException();
+        }
+
+        $request = 'DELETE FROM users
+                        WHERE email = :email';
+
+        $statement = $this->PDO->prepare($request);
+        $statement->bindParam(':email', $email);
         return $statement->execute();
     }
 }
