11c500b234395c30a0bbeaacf0e29929df44af75
sehnryr <youn@melois.dev>
2022-10-10 20:57:08 +0200

    add config file for database connection

---
+++ resources/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * PHP version 8.1.0
+ * 
+ * @author Youn Mélois <youn@melois.dev>
+ */
+
+// Basice parameters to connect to the database
+const DB_SERVER = '127.0.0.1';
+const DB_PORT = '5432';
+const DB_NAME = 'interpromos';
+const DB_USER = 'postgres';
+const DB_PASSWORD = '';
